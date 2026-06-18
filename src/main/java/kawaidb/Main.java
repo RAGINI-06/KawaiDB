f71a807f6b156299fead6aa6fd88171ecf3f5073
acer <nragini733@gmail.com>
2026-06-18 23:45:38 +0530

    Added interactive CLI

--- src/main/java/kawaidb/Main.java
+++ src/main/java/kawaidb/Main.java
@@ -1,4 +1,32 @@
 package kawaidb;
 
+//public class Main {
+//}
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to KawaiiDB!");
+        System.out.println("Type EXIT to quit.");
+
+        while (true) {
+
+            System.out.print("kawaiidb> ");
+
+            String command = scanner.nextLine();
+
+            if (command.equalsIgnoreCase("EXIT")) {
+                System.out.println("Goodbye!");
+                break;
+            }
+
+            System.out.println("You entered: " + command);
+        }
+
+        scanner.close();
+    }
 }
